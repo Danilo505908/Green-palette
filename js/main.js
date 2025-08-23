854aadf04406e09c1be569e377da328b5c4b7ae3
Danya <danahmaruk91@gmail.com>
2025-08-23 19:15:16 +0300

    new

--- js/main.js
+++ js/main.js
@@ -13,6 +13,6 @@ const swiper = new Swiper('.reviews__slider', {
 const menuBtn = document.querySelector('.menu__btn');
 const menuList = document.querySelector('.menu__list');
 
-menuBtn = addEventListener('click', () => {
+menuBtn.addEventListener('click', () => {
     menuList.classList.toggle('menu--open');
 });
